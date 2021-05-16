1b0eeb416939878929e6bf6cb956624ba42bbd6b
Moishi Netzer <moishinetzer@gmail.com>
2021-05-16 17:03:28 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,61 @@
+import "./styles.css";
+import { DataGrid } from "@material-ui/data-grid";
+import rows from "./data";
+
+export default function App() {
+  // console.log(rows);
+
+  const columns = [
+    { field: "FID", headerName: "FID" },
+    { field: "OBJECTID", headerName: "OBJECTID" },
+    { field: "NAME", headerName: "NAME" },
+    { field: "EASTING", headerName: "EASTING" },
+    { field: "NORTHING", headerName: "NORTHING" },
+    { field: "LINES", headerName: "LINES" },
+    { field: "NETWORK", headerName: "NETWORK" },
+    { field: "Zone", headerName: "Zone" },
+    { field: "x", headerName: "x" },
+    { field: "y", headerName: "y" }
+  ];
+
+  return (
+    <div className="App h-screen font-poppins px-20 ">
+      <h1 className="pt-10 text-4xl font-bold">Underground Line Data</h1>
+      <p className="pt-5 text-xl">
+        <span className="font-bold">Final solution: </span>
+        main.py is first used to filter and modify the csv file to the shape I
+        need it to be and filter out any of the rules given in the requirements
+        <br />
+        <br />
+        I am proficient in SQL however I have only learned it in an enclosed
+        environment and integrating it with an web application is something I
+        have not tried doing yet. I briefly tried to use a service like Firebase
+        for this however importing all the data was going to be quite tricky
+        based on the time constraints
+        <br />
+        <br />
+        (The edited data itself has not been used on this webpage as I was
+        having a very hard time converting the CSV to JSON, which would not be a
+        problem if I would have the time to properly look into integrating SQL
+        with the data and frontend with node)
+        <br />
+        <br />
+        After getting the formatted files now I can use material ui to create a
+        table to browse all the data and apply any filters on the data with
+        auto-pagination enabled. If I would have enough time I would have made
+        an collapsable accordian that shows the lines as an expanding section as
+        opposed to just being listed as they are now.
+        <br />
+        <br />
+        The table showing all the data is shown below.
+      </p>
+      <div className="h-full pt-10">
+        <DataGrid
+          rows={rows}
+          columns={columns}
+          getRowId={(row) => row["FID"]}
+        />
+      </div>
+    </div>
+  );
+}
---
+++ src/main.py
@@ -0,0 +1,48 @@
+import csv
+
+# Array of the final edited data with the first item being the headers
+filtered_csv = [["FID", "OBJECTID", "NAME", "EASTING", "NORTHING", "LINES", "NETWORK", "Zone", "x", "y"]]
+
+# Array of the allowed lines to check with all the lines
+allowed_lines = ["Central", "Northern", "Piccadilly", "Hammersmith & City", "District", "Circle", "Metropolitan",
+                 "Jubilee", "Waterloo & City", "Victoria", "Bakerloo"]
+
+# Process and filter the csv file
+with open('data.csv') as data:
+    csv_reader = csv.reader(data, delimiter=',')
+
+    # Separate the headers, don't need to save them just for readability
+    # Could also have saved here as first element but I think it may clearer there
+    headers = next(csv_reader)
+    for row in csv_reader:
+        edited_row = []
+        corrected_lines = []
+
+        # For each line in the lines field, check if it is allowed and append it 
+        # to an array as that is the shape of an JSON object
+        for line in row[5].split(", "):
+            if line in allowed_lines:
+                corrected_lines.append(line)
+
+        # As long as this row is for the underground we can use this row
+        if row[6] == "London Underground":
+            edited_row.append(int(row[0]))
+            edited_row.append(int(row[1]))
+            edited_row.append(row[2])
+            edited_row.append(int(row[3]))
+            edited_row.append(int(row[4]))
+            edited_row.append(corrected_lines)
+            edited_row.append(row[6])
+            edited_row.append(float(row[7]))
+            edited_row.append(float(row[4]))
+
+            # Now append that to the final array of data
+            filtered_csv.append(edited_row)
+
+# View the edited results
+print(filtered_csv)
+
+# Save results in csv file to use for API / website
+writer = csv.writer(open("editedData.csv", 'w', newline=''))
+for row in filtered_csv:
+    writer.writerow(row)
